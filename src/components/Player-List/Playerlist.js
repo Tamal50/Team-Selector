36cd78d29e8370223724ed47a8529ee6878d36f3
Tamal <tamal7971@gmail.com>
2021-03-02 21:40:35 +0600

    Checking

--- src/components/Player-List/Playerlist.js
+++ src/components/Player-List/Playerlist.js
@@ -4,18 +4,21 @@ import { faCoffee, faFutbol } from '@fortawesome/free-solid-svg-icons'
 import './playerList.css'
 
 const Player = (props) => {
-    const {image, full_name, salary, team, age} = props.player;
+    const {image, name, salary, team, age} = props.player;
     return (
         <div className="player">
         <div > 
             <img className="image" src={image} alt=""/>
         </div>
         <div className="details">
-            <h1>Name: {full_name}</h1>
+            <h1>Name: {name}</h1>
             <h2>Age: {age}</h2>
             <h3>Team: {team}</h3>
             <h4>salary: {salary}</h4>
-            <button className="button btn btn-danger"><FontAwesomeIcon icon={faFutbol} />ADD TO TEAM</button>
+            <button
+             className="button btn btn-danger"
+             onClick={() => props.handlePlayer(props.player)}
+             >          <FontAwesomeIcon icon={faFutbol} />ADD TO TEAM</button>
         </div>
     </div>
     );
--- src/components/Players/Player.js
+++ src/components/Players/Player.js
@@ -6,26 +6,31 @@ import './player.css'
 const Player = () => {
     const first15 = data.slice(0,15)
     const[player , setPlayer]= useState(first15);
+    const handlePlayer = () =>{
+        console.log('product added')
+    }
 
+    
     return (
         <div className = "container">
             <div className="player-container">
                 <h1>Choose Player</h1>
                          
                     {
-                        player.map (pt => <Playerlist player={pt}></Playerlist>)
+                        player.map (pt => <Playerlist
+                            handlePlayer = {handlePlayer}
+                             player={pt}>
+
+                             </Playerlist>)
                     }
                
             
             </div>
             <div className="playerlist-container">
-                    <h3>Adding to team</h3>
+                    <h3>Final Team</h3>
+                   
             </div>
         </div>
     );
 };
-
 export default Player;
-
-
-
